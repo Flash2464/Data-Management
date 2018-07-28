349075d836d3b03f9de88644778f00ba622f986a
shubham agarwal <shubham99100@gmail.com>
2018-07-28 15:49:02 +0530

    Initial commit

---
+++ src/main/java/com/beans/userBO.java
@@ -0,0 +1,56 @@
+package com.beans;
+
+public class userBO 
+{
+	private String firstname;
+	private String middlename;
+	private String lastname;
+	private String age;
+	private String dateofbirth;
+	private String city;
+	private String country;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getMiddlename() {
+		return middlename;
+	}
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getDateofbirth() {
+		return dateofbirth;
+	}
+	public void setDateofbirth(String dateofbirth) {
+		this.dateofbirth = dateofbirth;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
---
+++ src/main/java/com/services/MainServer.java
@@ -0,0 +1,9 @@
+package com.services;
+
+public class MainServer {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Main Server Initiated");
+	}
+}
